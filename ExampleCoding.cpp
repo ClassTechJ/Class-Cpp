f835b6c6105da070a913339c91dd4ed845c17230
ClassTechJ <167958552+ClassTechJ@users.noreply.github.com>
2024-04-24 00:19:14 -0600

    Create ExampleCoding.cpp
    Creando el archivo cpp correspondiente a la clase del día 24 de abril del 2024.
    Estructura básica de un codigo.

---
+++ ExampleCoding.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int Ndia, dia = 24;
+    cout << "El dia de hoy es " << dia << " de abril" << endl;
+    cout << "Ma"<<char(164)<<"ana que dia sera?" << endl;
+    cin >> Ndia;
+    cout << "Cierto, ma"<<char(164)<<"ana sera " << Ndia << " de abril" << endl;
+    system("pause");
+    return 0;
+}
